4450178c4b91b27084ee8f761cf2d96be4b1b7e8
Ashley Taylor <blueberrypea@gmail.com>
2023-04-18 11:38:19 -0400

    fixing files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+//Display three potential products side-by-side-by-side; 3 diff images
+
+//Store each anonymous vote, calculate totals, and visually display the results.
+
+//Do not allow any results to be shown to users until there have been a total of 25 selections made.
